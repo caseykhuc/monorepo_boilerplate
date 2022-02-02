c8ee8e825d44a8e1ab191535085745e8751abfe2
khuctrang <khuctrang1812@gmail.com>
2022-02-02 17:22:07 +0700

    feat: add a dummy react package

---
+++ packages/component-libs/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve('dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx|ts|tsx)$/,
+      exclude: /node_modules/,
+      use: "babel-loader"
+    }]
+  }
+}
