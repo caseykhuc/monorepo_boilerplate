b13c991f364b65d7149d9bc51f2cc4b34f9f75af
khuctrang <khuctrang1812@gmail.com>
2022-02-02 20:48:34 +0700

    feat: setup eslint with prettier

--- packages/component-libs/webpack.config.js
+++ packages/component-libs/webpack.config.js
@@ -1,25 +1,34 @@
 const path = require('path');
-const webpack = require('webpack');
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+// const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
 const htmlPlugin = new HtmlWebPackPlugin({
- template: "./public/index.html",
- filename: "./index.html"
+  template: './public/index.html',
+  filename: './index.html'
 });
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
+  entry: './src/index.jsx',
+  mode: 'development',
   output: {
-    filename: "bundle.js",
+    filename: 'bundle.js',
     path: path.resolve('dist'),
     publicPath: '/'
   },
   module: {
-    rules: [{
-      test: /\.(js|jsx|ts|tsx)$/,
-      exclude: /node_modules/,
-      use: "babel-loader"
-    }]
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
   },
-  plugins: [htmlPlugin]
-}
+  plugins: [htmlPlugin],
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, 'src/components')
+    },
+    extensions: ['.js', '.jsx']
+  }
+};
