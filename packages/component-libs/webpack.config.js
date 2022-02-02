57c3b22ec49df357f467b1195aa51c64a28b9992
khuctrang <khuctrang1812@gmail.com>
2022-02-02 17:32:15 +0700

    feat: add html-webpack-plugin and use webpack-dev-server to support live loading

--- packages/component-libs/webpack.config.js
+++ packages/component-libs/webpack.config.js
@@ -1,5 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const htmlPlugin = new HtmlWebPackPlugin({
+ template: "./public/index.html",
+ filename: "./index.html"
+});
 
 module.exports = {
   entry: "./src/index.js",
@@ -15,5 +20,6 @@ module.exports = {
       exclude: /node_modules/,
       use: "babel-loader"
     }]
-  }
+  },
+  plugins: [htmlPlugin]
 }
